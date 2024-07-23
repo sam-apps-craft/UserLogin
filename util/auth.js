3132f77957842971a0ef0c660fcc7f763aef30d9
sam1246 <sam@apps-craft.com>
2024-07-23 16:50:40 +0100

    token input

--- util/auth.js
+++ util/auth.js
@@ -3,8 +3,7 @@ import axios from "axios";
 const API_KEY = "AIzaSyCcqk9Avi0szoS0klDveF7EwUwQ_hl4S1Y";
 
 async function authenticate(mode, email, password) {
-  const url =
-    `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
 
   const response = await axios.post(url, {
     email: email,
@@ -12,12 +11,14 @@ async function authenticate(mode, email, password) {
     returnSecureToken: true,
   });
 
-  console.log(response.data);
+  const token = response.data.idToken;
+
+  return token;
 }
 
-export async function createUser(email, password) {
-  await authenticate("signUp", email, password);
+export function createUser(email, password) {
+  return authenticate("signUp", email, password);
 }
-export async function login(email, password) {
-  await authenticate("signInWithPassword", email, password);
+export function login(email, password) {
+  return authenticate("signInWithPassword", email, password);
 }
