a7903fc90347da623b4cc27ae6f314926f27e994
sam1246 <sam@apps-craft.com>
2024-08-19 13:26:02 +0100

    Corrections

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,29 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessaged] = useState("");
+
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://userlogin-fe55e-default-rtdb.firebaseio.com/message.json?auth" +
+          token
+      )
+      .then((response) => {
+        setFetchedMessaged(response.data);
+      });
+  }, [token]);
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
